297753158c2455ae984c7b3fc04cf8b89a5aadac
MohammedToshif <toshif92612@gmail.com>
2024-02-21 21:38:54 +0530

    ...

---
+++ 01_The_CLI.js
@@ -0,0 +1,42 @@
+
+            //----- (01) - (Anatomy of the Command Line Interface)
+
+// pwd      // (present working directory)            
+
+// pwd
+
+// pwd --help
+
+// clear
+
+// //-------------------------------
+
+// cd /home                 // (cd - for change directory)
+
+// pwd
+
+// cd /home /toshif
+
+// cd..
+
+// pwd
+
+// cd /home /toshif
+
+// cd../..
+
+// pwd
+
+// pwd
+
+// pwd 
+
+// cd home
+
+// cd toshif
+
+// cd..
+
+// cd../ toshif
+
+// cd../ toshif /../ toshif
