ca97c48088cfc629f484d952121aa9524c5c3853
MohammedToshif <toshif92612@gmail.com>
2024-02-23 19:53:36 +0530

    ...

--- 01_The_CLI.js
+++ 01_The_CLI.js
@@ -40,3 +40,222 @@
 // cd../ toshif
 
 // cd../ toshif /../ toshif
+
+
+            //----- (02) - (CLI Directories & Arguments)
+
+
+//  ls
+
+// cd ..
+
+// pwd
+
+// ls
+
+// pwd
+
+// ls home
+
+// ls --help
+
+// clear
+
+//--------------------------------------------
+
+// echo hi
+
+// echo welcome to frontend masters
+
+// echo "welcome to frontend masters"
+
+
+
+// which ls         (/bin/ls) bin - directory
+
+// cd /bin
+
+// ls
+
+
+// cd /home
+
+// pwd
+
+// clear
+
+
+            //----- (03) - (Flags)
+
+// pwd --help
+
+// clear
+
+
+//-----------------------------------------------------------
+
+// echo --help      (--help)
+
+// man echo
+
+// echo --help      (--help)
+
+// clear
+
+
+//-----------------------------------------------------------
+
+// ls
+
+// ls -l    (-l = another kind of flag, it is a long form output) (out = total 4)
+
+// ls -l
+
+// ls -l -a     (-a = show hidden files in directory)
+
+// touch .hidden-files      (touch = just create a file)
+
+// ls -l
+
+
+//---------------------------------------------------------------
+
+
+// ls --help
+
+// ls --all
+
+// ls -a    (-a = shorter way of --all)
+
+// ls -l -a
+
+// ls --all -l
+
+
+//---------------------------------------------------
+
+// ls       (Home snap)
+
+// ls --ignore=snap     (Home)
+
+// ls --ignore snap     (Home)
+
+// ls --ignore=snap --ignore=Home 
+
+// clear
+
+
+//--------------------------------------------------------
+
+// ls -lsah     (this give you full output and show all hidden files)
+
+//  ls --help
+
+// clear
+
+
+// /-----------------------------
+
+// ls --ignore snap (Home)
+
+// ls -I snap   (Home)
+
+// ls --help
+
+// clear
+
+
+//----------------------------------------------------
+
+// pwd
+
+// ls snap      (mltipass-sshfs)
+
+// ls -lsah snap    
+
+// ls snap -lsah
+
+// ls -lsah snap
+
+// ls snap -lsah
+
+
+
+            //----- (04) - (CLI Search)
+
+
+// pwd
+
+// cd /
+
+// ls       (for root directory)
+
+//  cd ~        ( ~ represent your own home directory)
+
+// pwd 
+
+// cd ~/snap    (its going to home directory)
+
+// pwd
+
+// cd ~
+
+// cd ..
+
+// cd ..
+
+// cd ~
+
+// cd snap/
+
+// cd ..
+
+// cd /
+
+// pwd
+
+// cd ~
+
+// clear
+
+
+
+//--------------------------------------------------------
+
+// ls -lsah snap
+
+//  clear
+
+
+//------------------------------------------------------
+
+// cd home/
+
+// cd..
+
+// ls
+
+// cd home/toshif/  (show all the directories)
+
+// cd home/toshif/snap/multipass-sshfs/     (out = common/ current/)
+
+// ls
+
+// py   (the potential prgrm that are available to me that i can run with py.)
+
+// python3
+
+
+// if you hit Ctrl+R    = ls -lsah snap
+
+// if you hit Ctrl+R    = ls snap/
+
+
+
+
+
+
+
+
+
+
