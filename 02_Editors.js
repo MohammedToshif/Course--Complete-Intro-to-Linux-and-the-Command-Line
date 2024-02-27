f4a483cdfbe4331ffe762fb50c3a5d3b9b89f928
MohammedToshif <toshif92612@gmail.com>
2024-02-27 22:19:41 +0530

    new wrk

---
+++ 02_Editors.js
@@ -0,0 +1,55 @@
+
+            //-----(01) - (Nano)
+
+// :~$  kill -l
+
+ // :~$  cd ~
+
+// :~$  nano textfile.txt    ====> GUN nano 2.9.3    textfile.txt
+
+
+
+// - this is a textt file
+
+// - I can write whatever I want
+
+// - this is so cool 
+
+
+
+            //-----(02) - (VIM: A Brief History)
+
+
+ // :~$  ed ed.txt
+
+ /**
+  * - output: ed.text: NO such file or directory
+  * - sdfklj
+  * - ?
+  * - lk
+  * - ?
+  * - ^C
+  * - ?
+  * 
+  * -> press CTL+D exit 
+  * 
+  */
+
+ // :~$  ls
+
+ // :~$  ls -lsah
+
+
+// :~$  ex textfile.txt
+
+// ==> "textfile.txt" 8L, 88C
+// ==> Entering Ex mode. Type "visual" to go to Normal mode.
+
+// :visual
+
+// E501: At end-of-file
+
+
+// :~$  vi textfile.txt
+
+// :~$  clear
